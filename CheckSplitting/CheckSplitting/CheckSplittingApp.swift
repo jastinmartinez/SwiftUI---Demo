120a8b93c413b2e3aed397d41f9ea0f3ab1d1c1f
Jastin Martiez <martinez.jastin@hotmail.com>
2021-10-27 16:01:11 -0400

    WeSplit init
    demo project for split account and tips

---
+++ CheckSplitting/CheckSplitting/CheckSplittingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSplittingApp.swift
+//  CheckSplitting
+//
+//  Created by Jastin on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CheckSplittingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckSplitting/CheckSplitting/ContentView.swift
@@ -0,0 +1,83 @@
+//
+//  ContentView.swift
+//  CheckSplitting
+//
+//  Created by Jastin on 26/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmount:Double = 0.0
+    @State private var amountOfPerson:Int = 2
+    @State private var percenteOfTip:Int = 0
+    
+    private var totalPerPerson:Double {
+         
+       return  ( checkAmount + ( ( checkAmount / 100 ) * Double(percenteOfTip) ) ) / Double(amountOfPerson)
+        
+    }
+    
+    private let tips = (0...5).map({$0 * 5})
+    private let defaultCurrencyCode = Locale.current.currencyCode ?? "USD"
+    
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                
+                Section {
+                    TextField("Chek Amount", value: $checkAmount, format: .currency(code: defaultCurrencyCode))
+                        .keyboardType(.decimalPad)
+                }header: {
+                    Text("Check Amount")
+                }
+                
+                Section {
+                    Picker("Persons", selection: $amountOfPerson) {
+                        ForEach(2..<100,id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                }header: {
+                    Text("Number Of Person")
+                }
+                
+                Section {
+                    Picker("", selection: $percenteOfTip) {
+                        ForEach(tips,id: \.self) {
+                            Text($0,format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }header: {
+                    Text("Tips")
+                }
+                
+                Section {
+                    HStack()
+                    {
+                        Spacer()
+                        Text(totalPerPerson,format: .currency(code: defaultCurrencyCode))
+                            .font(.largeTitle)
+                            .foregroundColor(.green)
+                    }
+                    
+                }header: {
+                    Text("Result")
+                }
+                
+                
+            }
+            .navigationTitle("WeSplit")
+
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
