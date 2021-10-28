7258857f55941e2c4d1fd2752c88df8940bb0508
Jastin Martiez <martinez.jastin@hotmail.com>
2021-10-27 20:35:41 -0400

    init

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jastin on 27/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
