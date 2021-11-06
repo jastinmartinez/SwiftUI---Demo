50b593b26ebeb6cc3301c4fc9e0d0e646cadea8c
Jastin Martiez <martinez.jastin@hotmail.com>
2021-11-06 18:11:35 -0400

    init
    initial files

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jastin on 6/11/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
