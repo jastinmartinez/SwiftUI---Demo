6b1bc6239337d037d04347c070650d48432ddde2
Jastin Martinez <martinez.jastin@hotmail.com>
2023-07-13 23:43:03 -0400

    Project initialization
    include basic folder structure and also initial test

---
+++ IDine/IDine/IDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDineApp.swift
+//  IDine
+//
+//  Created by Jastin on 13/7/23.
+//
+
+import SwiftUI
+
+@main
+struct IDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
