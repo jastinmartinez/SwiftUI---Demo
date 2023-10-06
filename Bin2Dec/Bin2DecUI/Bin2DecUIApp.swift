d4a26b504ef405546ad20fed45fe0983d95f42cb
Jastin Martinez <martinez.jastin@hotmail.com>
2023-10-05 20:18:32 -0400

    init UI Module

---
+++ Bin2Dec/Bin2DecUI/Bin2DecUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bin2DecUIApp.swift
+//  Bin2DecUI
+//
+//  Created by Jastin on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Bin2DecUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
