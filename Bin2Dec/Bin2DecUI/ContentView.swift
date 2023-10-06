119c10c8af30bb5db8e0f2d2baa14b2c3d5983fa
Jastin Martinez <martinez.jastin@hotmail.com>
2023-10-06 12:12:15 -0400

    include some initial design for values input

--- Bin2Dec/Bin2DecUI/ContentView.swift
+++ Bin2Dec/Bin2DecUI/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text: String = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            VStack(spacing: 15) {
+                Spacer()
+                Label(
+                    title: { Text("Binary").font(.title) },
+                    icon: { Image(.binary)
+                            .resizable()
+                            .frame(width: 40,
+                                   height: 40) }
+                )
+                TextField("0101010", text: $text)
+                    .font(.title2)
+                    .fixedSize()
+                
+                Button("Convert") {
+                    print("")
+                }
+                .padding(7)
+                .background(.blue)
+                .foregroundStyle(.white)
+                .cornerRadius(5)
+                Spacer()
+                Label(
+                    title: { Text("\(0)")
+                            .font(.title)
+                        .fontWeight(.semibold) },
+                    icon: { Image(.decimal)
+                            .resizable()
+                            .frame(width: 40,
+                                   height: 40) }
+                )
+            }
+        }.navigationTitle("Bin2Dec")
+            .navigationBarTitleDisplayMode(.inline)
     }
 }
 
