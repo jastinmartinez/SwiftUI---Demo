e096c248326e97dacfb1aeed874debf508b406f0
Jastin Martinez <martinez.jastin@hotmail.com>
2023-10-07 10:53:27 -0400

    restore UI and also include new support from mac OS target to IPhone

---
+++ Bin2Dec/Bin2DecUI/Bin2DecUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bin2DecUIApp.swift
+//  Bin2DecUI
+//
+//  Created by Jastin on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Bin2DecUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
