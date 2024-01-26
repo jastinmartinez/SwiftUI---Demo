dc66317ad78c202654d3a5fcf9d12c5f1cc286a4
Jastin Martinez <martinez.jastin@hotmail.com>
2024-01-26 00:06:38 -0400

    initial we split project

---
+++ WeSplit/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jastin on 22/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
