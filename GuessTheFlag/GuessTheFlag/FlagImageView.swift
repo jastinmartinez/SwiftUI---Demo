e8da7380543b015b2740dd30b07ce1b1bfd69f02
Jastin Martinez <martinez.jastin@hotmail.com>
2024-02-11 13:43:51 -0400

    Challenge -> Add `FlagImageView`

---
+++ GuessTheFlag/GuessTheFlag/FlagImageView.swift
@@ -0,0 +1,18 @@
+//
+//  FlagImageView.swift
+//  GuessTheFlag
+//
+//  Created by Jastin on 11/2/24.
+//
+
+import SwiftUI
+
+struct FlagImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlagImageView()
+}
