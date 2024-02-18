01bc88a5b5d1c5c77492bc989e9d84971a0e8ffc
Jastin Martinez <martinez.jastin@hotmail.com>
2024-02-18 18:30:23 -0400

    set up test target and set initial test

---
+++ RockPaperScissors/RockPaperScissorsTests/RockPaperScissorsTests.swift
@@ -0,0 +1,15 @@
+//
+//  RockPaperScissorsTests.swift
+//  RockPaperScissorsTests
+//
+//  Created by Jastin on 18/2/24.
+//
+
+import XCTest
+
+final class RockPaperScissorsTests: XCTestCase {
+
+    func test() {
+        XCTFail("Fail")
+    }
+}
