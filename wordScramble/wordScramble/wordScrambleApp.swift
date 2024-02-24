46fe9955e0af49ca3547723ff9fdcc98db34e004
Jastin Martinez <martinez.jastin@hotmail.com>
2024-02-24 13:04:16 -0400

    project init

---
+++ wordScramble/wordScramble/wordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordScrambleApp.swift
+//  wordScramble
+//
+//  Created by Jastin on 23/2/24.
+//
+
+import SwiftUI
+
+@main
+struct wordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
