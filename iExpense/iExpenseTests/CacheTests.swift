99f14f451c703921e485f11e4842738006f085a3
Jastin Martinez <martinez.jastin@hotmail.com>
2024-03-17 19:39:07 -0400

    initial design

---
+++ iExpense/iExpenseTests/CacheTests.swift
@@ -0,0 +1,43 @@
+//
+//  Cache.swift
+//  iExpenseTests
+//
+//  Created by Jastin on 17/3/24.
+//
+
+import XCTest
+
+final class Cache {
+    
+    private let storer: UserDefaults
+    
+    init(storer: UserDefaults) {
+        self.storer = storer
+    }
+    
+    func save(_ item: Encodable, for key: String, completion: (Error?) -> Void) {
+        do {
+            let data = try JSONEncoder().encode(item)
+            storer.setValue(data, forKey: key)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
+      
+    }
+}
+
+final class CacheTests: XCTestCase {
+
+    
+    func test_saveCache_storeValues() {
+        let item = "Data"
+        let sut = Cache(storer: .standard)
+        let exp = expectation(description: "wait for saved cache")
+        sut.save(item, for: "data", completion: { error in
+            XCTAssertNil(error)
+            exp.fulfill()
+        })
+        wait(for: [exp], timeout: 1.0)
+    }
+}
