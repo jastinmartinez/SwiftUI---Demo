6e6892b4a54ecf5e8ee04ed998a160db5d1551f7
Jastin Martinez <martinez.jastin@hotmail.com>
2024-03-18 18:53:00 -0400

    files

--- iExpense/iExpense/model/ExpenseItem.swift
+++ iExpense/iExpense/model/ExpenseItem.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct ExpenseItem: Identifiable, Codable {
+    let id: UUID
+    let name: String
+    let type: String
+    let value: Double
+    
+    init(name: String, type: String, value: Double) {
+        self.id = UUID()
+        self.name = name
+        self.type = type
+        self.value = value
+    }
+}
--- iExpense/iExpense/view/ExpenseItemView.swift
+++ iExpense/iExpense/view/ExpenseItemView.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ExpenseItemView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var name = ""
+    @State private var type = ""
+    @State private var value = 0.0
+    private let types = ["Personal", "Business"]
+    let expense: Expense
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("name", text: $name)
+                Picker("type", selection: $type) {
+                    ForEach(types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                TextField("value", value: $value, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add")
+            .toolbar {
+                Button("save") {
+                    expense.add(ExpenseItem(name: name, type: type, value: value))
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ExpenseItemView(expense: Expense())
+}
+
