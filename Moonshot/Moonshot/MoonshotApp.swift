7b50b05c1ca3b739d5c502127f2b166e3e03e215
Jastin Martinez <martinez.jastin@hotmail.com>
2024-03-19 11:33:31 -0400

    ini

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Jastin on 18/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
